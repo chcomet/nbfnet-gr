4a042f2a0d25a9c2acad33db39a6e5ab6011e552
Hui <chengh@in.tum.de>
2023-10-12 21:59:11 +0200

    fix run

--- script/run.py
+++ script/run.py
@@ -4,7 +4,6 @@ import math
 import pprint
 
 import torch
-from torch.profiler import profile, record_function, ProfilerActivity
 
 from torchdrug import core, models
 from torchdrug.utils import comm
@@ -86,16 +85,6 @@ if __name__ == "__main__":
     dataset = core.Configurable.load_config_dict(cfg.dataset)
     solver = util.build_solver(cfg, dataset)
 
-    if args.profile:
-        print("running train/val with profiler")
-        
-        with profile(activities=[ProfilerActivity.CUDA],
-                     profile_memory=True, record_shapes=True) as prof:
-            train_and_validate(cfg, solver)
-        
-        print("profiler output")
-        print(prof.key_averages().table(sort_by="self_cuda_memory_usage", row_limit=50))
-    else:
-        train_and_validate(cfg, solver)
+    train_and_validate(cfg, solver)
     
     test(cfg, solver)
