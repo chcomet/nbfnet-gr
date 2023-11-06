e5472e3424826f31fabf1e6b83b7e249158fd74b
Hui <chengh@in.tum.de>
2023-11-06 13:01:22 +0100

    checkpoints - continue test of lnctardv1

--- script/test.py
+++ script/test.py
@@ -11,54 +11,6 @@ from torchdrug.utils import comm
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from nbfnet import dataset, layer, model, task, util
 
-def solver_load(checkpoint, load_optimizer=True):
-
-    if comm.get_rank() == 0:
-        logger.warning("Load checkpoint from %s" % checkpoint)
-    checkpoint = os.path.expanduser(checkpoint)
-    state = torch.load(checkpoint, map_location=solver.device)
-    # some issues with loading back the fact_graph and graph
-    # remove
-    state["model"].pop("fact_graph")
-    state["model"].pop("graph")
-    if "undirected_fact_graph" in state["model"]:
-        state["model"].pop("undirected_fact_graph")
-    # load without
-    solver.model.load_state_dict(state["model"], strict=False)
-
-    if load_optimizer:
-        solver.optimizer.load_state_dict(state["optimizer"])
-        for state in solver.optimizer.state.values():
-            for k, v in state.items():
-                if isinstance(v, torch.Tensor):
-                    state[k] = v.to(solver.device)
-
-    comm.synchronize()
-
-
-def train_and_validate(cfg, solver):
-    if cfg.train.num_epoch == 0:
-        return
-
-    step = math.ceil(cfg.train.num_epoch / 10)
-    best_result = float("-inf")
-    best_epoch = -1
-
-    for i in range(0, cfg.train.num_epoch, step):
-        kwargs = cfg.train.copy()
-        kwargs["num_epoch"] = min(step, cfg.train.num_epoch - i)
-        solver.model.split = "train"
-        solver.train(**kwargs)
-        solver.save("model_epoch_%d.pth" % solver.epoch)
-        solver.model.split = "valid"
-        metric = solver.evaluate("valid")
-        result = metric[cfg.metric]
-        if result > best_result:
-            best_result = result
-            best_epoch = solver.epoch
-
-    return solver
-
 
 def test(cfg, solver):
     solver.model.split = "valid"
@@ -94,6 +46,6 @@ if __name__ == "__main__":
     metric = solver.evaluate("valid")
     result = metric[cfg.metric]
 
-    solver_load("/root/model_epoch_7.pth")
+    util.solver_load(solver, "/root/model_epoch_7.pth")
 
     test(cfg, solver)
